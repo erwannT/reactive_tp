8dc7e9e033564552b58a3519c89ba70f107ee76a
Maxime Odye <maxime.odye@zenika.com>
2018-03-26 17:07:17 +0200

    refac(polish): clean up

--- src/test/java/com/point/tp/BackPressureTest.java
+++ src/test/java/com/point/tp/BackPressureTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 import org.reactivestreams.Subscription;
 import reactor.core.publisher.BaseSubscriber;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.FluxSink;
 import reactor.core.scheduler.Schedulers;
 
 import java.time.LocalDateTime;
@@ -14,65 +15,84 @@ import java.time.LocalDateTime;
 public class BackPressureTest {
 
     @Test
-    public void test() throws InterruptedException {
+    public void backPressureExample() throws InterruptedException {
         Flux
-            .create(stringFluxSink -> new Thread(() -> {
-                log.info("Hello world");
-                long i = 0;
-                while (true) {
-
-                    log.info("Generate : " + i);
-                    stringFluxSink.next(cookBurger(i));
-                    try {
-                        Thread.sleep(1000);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                    i++;
-                }
-            }).start())
-
-            .publishOn(Schedulers.newElastic("Client"))
-            .map(o -> (Burger) o)
+                .create(stringFluxSink -> new Thread(() ->
+                        fluxSink(stringFluxSink)
+                ).start())
+                .publishOn(Schedulers.newElastic("Client"))
+                .map(o -> (Burger) o)
 //            .onBackpressureDrop(burger -> {
 //                System.out.println("Drop " +burger.getId());
 //            })
 //            .onBackpressureBuffer(4, burger -> {
 //                log.info("Overflow sur " + burger.getId());
 //            })
-            .onBackpressureLatest()
-            .subscribeOn(Schedulers.newElastic("Cook"))
-            .subscribe(new BaseSubscriber<Burger>() {
-                @Override
-                protected void hookOnSubscribe(Subscription subscription) {
-                    request(1);
-                }
-
-                @Override
-                protected void hookOnNext(Burger value) {
-                    log.info("Display : " + value.getId());
-                    if( value.isHot(LocalDateTime.now()))
-                    {
-                        log.info("Good burger !!!");
-                    }
-                    else{
-                        log.error("This burger is cold");
-                    }
-                    try {
-                        Thread.sleep(5000);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                    request(1);
-                }
-            })
-        ;
+                .onBackpressureLatest()
+                .subscribeOn(Schedulers.newElastic("Cook"))
+                .subscribe(burgerBaseSubscriber());
 
         Thread.sleep(1000000);
     }
 
-    public Burger cookBurger(Long id) {
+    /**
+     * Each second, generate a new burger order
+     *
+     * @param stringFluxSink
+     */
+    private void fluxSink(FluxSink<Object> stringFluxSink) {
+        try {
+            log.info("Hello, we will cook gorgeous burgers");
+            long i = 0;
+            while (true) {
+                log.info("Generate : " + i);
+                stringFluxSink.next(cookBurger(i));
+                Thread.sleep(1000);
+                i++;
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 
+    /**
+     * Burger subscriber.
+     * Every second, request one burger.
+     *
+     * @return
+     */
+    private BaseSubscriber<Burger> burgerBaseSubscriber() {
+        return new BaseSubscriber<Burger>() {
+            @Override
+            protected void hookOnSubscribe(Subscription subscription) {
+                request(1);
+            }
+
+            @Override
+            protected void hookOnNext(Burger value) {
+                try {
+                    log.info("Display : " + value.getId());
+                    if (value.isHot(LocalDateTime.now())) {
+                        log.info("Good burger !!!");
+                    } else {
+                        log.error("This burger is cold");
+                    }
+                    Thread.sleep(5000);
+                    request(1);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+    }
+
+    /**
+     * Cook a burger, boilerplate
+     *
+     * @param id
+     * @return
+     */
+    private Burger cookBurger(Long id) {
         Burger burger = new Burger();
         burger.setRelease(LocalDateTime.now());
         burger.setId(id);
